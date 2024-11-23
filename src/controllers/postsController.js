3cd911c73991333cc450bc080f7a02e90dc6e5ca
Vinicius <vinicius0793@gmail.com>
2024-11-22 23:44:50 -0300

    Implementação do método Post

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,4 @@
-import getTodosPosts from "../models/postsModel.js";
+import { getTodosPosts, criarPost } from "../models/postsModel.js";
 
 export async function listarPosts(req, res) {
   // Chama a função para buscar os posts
@@ -6,4 +6,17 @@ export async function listarPosts(req, res) {
 
   // Envia os posts como resposta em formato JSON com status 200 (sucesso)
   res.status(200).json(posts);
+}
+
+export async function postarNovoPost(req, res) {
+  const novoPost = req.body;
+  
+  try {
+    const postCriado = await criarPost(novoPost);
+
+    res.status(200).json(postCriado);
+  } catch (erro) {
+    console.error(erro.message);
+    res.status(500).json({"Erro":"Falha na requisição."})
+  }
 }
--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -4,7 +4,7 @@ import conectarAoBanco from "../config/dbconfig.js";
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 // Função assíncrona para buscar todos os posts de uma coleção específica
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
   // Seleciona o banco de dados 'imersao-instabytes'
   const db = conexao.db("imersao-instabytes");
 
@@ -13,4 +13,14 @@ export default async function getTodosPosts() {
 
   // Retorna um array com todos os documentos da coleção
   return colecao.find().toArray();
+}
+
+export async function criarPost(novoPost) {
+  // Seleciona o banco de dados 'imersao-instabytes'
+  const db = conexao.db("imersao-instabytes");
+
+  // Seleciona a coleção 'posts' dentro do banco de dados
+  const colecao = db.collection("posts");
+
+  return colecao.insertOne(novoPost);
 }
