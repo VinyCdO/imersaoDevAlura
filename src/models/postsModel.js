c48a999d2840d87edce6c90ba6fac7987ac4c69a
Vinicius <vinicius0793@gmail.com>
2024-11-24 02:32:09 -0300

    Inclusão de salvamento de url da imagem com nome baseado no ID do registro

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbconfig.js";
 
 // Conecta ao banco de dados MongoDB e retorna uma promessa
@@ -23,4 +24,16 @@ export async function criarPost(novoPost) {
   const colecao = db.collection("posts");
 
   return colecao.insertOne(novoPost);
+}
+
+export async function atualizarPost(id, post) {
+  // Seleciona o banco de dados 'imersao-instabytes'
+  const db = conexao.db("imersao-instabytes");
+
+  // Seleciona a coleção 'posts' dentro do banco de dados
+  const colecao = db.collection("posts");
+
+  const objId = ObjectId.createFromHexString(id);
+  
+  return colecao.updateOne({_id: new ObjectId(objId)}, {$set:post});
 }
