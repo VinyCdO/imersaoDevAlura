04f5fb12382d51e8c7b1631213c976f60ca20732
Vinicius <vinicius0793@gmail.com>
2024-11-25 00:28:06 -0300

    Integração com serviço Gemini AI da Google para geração de descrição das imagens

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 import { getTodosPosts, criarPost, atualizarPost } from "../models/postsModel.js";
+import gerarDescricaoComGemini from "../services/geminiService.js";
 
 export async function listarPosts(req, res) {
   // Chama a função para buscar os posts
@@ -43,15 +44,19 @@ export async function uploadImagem(req, res) {
 export async function atualizarNovoPost(req, res) {
   const id = req.params.id;
   const urlImagem = `http://localhost:3000/${id}.png`
-  const post = {
-    imgUrl: urlImagem,
-    descricao: req.body.descricao,
-    alt: req.body.alt
-  }
-
+  
   try {
-    const postCriado = await atualizarPost(id, post);
+    const imgBuffer = fs.readFileSync(`uploads/${id}.png`)
+    const descricao = await gerarDescricaoComGemini(imgBuffer);
 
+    const post = {
+      imgUrl: urlImagem,
+      descricao: descricao,
+      alt: req.body.alt
+    }
+
+    const postCriado = await atualizarPost(id, post);
+    
     res.status(200).json(postCriado);
   } catch (erro) {
     console.error(erro.message);
