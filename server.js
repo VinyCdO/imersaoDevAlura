e254b0f3c20b195d1fabf6b76364142b0be06425
Vinicius <vinicius0793@gmail.com>
2024-11-28 02:23:05 -0300

    ajuste porta do servidor

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.use(express.static("uploads"));
 routes(app);
 
 // Inicia o servidor na porta 3000 e exibe uma mensagem no console
-app.listen(3000, () => {
+app.listen(80, () => {
   console.log("Servidor escutando...");
 });
