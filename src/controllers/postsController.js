94045cf59e58f116756c3795f7c5aaa298ce46a3
Vinicius <vinicius0793@gmail.com>
2024-11-28 02:34:07 -0300

    Ajuste de porta

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -43,7 +43,7 @@ export async function uploadImagem(req, res) {
 
 export async function atualizarNovoPost(req, res) {
   const id = req.params.id;
-  const urlImagem = `http://localhost:3000/${id}.png`
+  const urlImagem = `http://localhost:80/${id}.png`
   
   try {
     const imgBuffer = fs.readFileSync(`uploads/${id}.png`)
